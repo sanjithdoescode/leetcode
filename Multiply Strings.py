d28163c4779375c4f00e1557a5845ec4f9afdae7
Sanjith Karthikeyan <sanjithkarthik16@gmail.com>
2025-01-09 21:29:20 +0530

    Create Multiply Strings.py

---
+++ Multiply Strings.py
@@ -0,0 +1,9 @@
+'''
+Problem No. 43 
+Multiply Strings
+Medium difficulty btw lmao
+'''
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1) * int(num2))
