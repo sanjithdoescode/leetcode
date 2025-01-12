c9abd1affeaf41abdcc6a406d94508ad8a2b4d74
Sanjith Karthikeyan <sanjithkarthik16@gmail.com>
2025-01-12 19:37:33 +0530

    Create Search Insert Position.go

---
+++ Search Insert Position.go
@@ -0,0 +1,21 @@
+// Day - 12
+// Problem - 35
+// Search Insert Position
+
+func searchInsert(nums []int, target int) int {
+	l := 0
+	r := len(nums) - 1
+	for l <= r {
+		m := l + (r-l)/2
+		v := nums[m]
+		switch {
+		case v < target:
+			l = m + 1
+		case v > target:
+			r = m - 1
+		default:
+			return m
+		}
+	}
+	return l
+}
