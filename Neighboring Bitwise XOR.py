ab108e1f24aca11eb1dc152a6b0ce555e8df9a2a
Sanjith Karthikeyan <sanjithkarthik16@gmail.com>
2025-01-17 14:38:57 +0530

    Create Neighboring Bitwise XOR.py

---
+++ Neighboring Bitwise XOR.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def doesValidArrayExist(self, derived):
+        """
+        :type derived: List[int]
+        :rtype: bool
+        """
+        n = len(derived)
+        xor_sum = 0
+    
+        for num in derived:
+            xor_sum ^= num
+        
+        return xor_sum == 0
+        
